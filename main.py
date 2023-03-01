3498ff9b2e54cdcb7ba160aaf4477221c501af1f
milos <milos.991@gmail.com>
2023-03-01 09:28:41 +0100

    stuff works and stuff

--- main.py
+++ main.py
@@ -1,48 +1,90 @@
 import json
+import pprint
+import typing
 
-from durable import engine
+from durable import engine, lang
 
-RULES = """
-{
-  "test": {
-    "r_0": {
-      "all": [
-        {
-          "m": {
-            "subject": "World"
-          }
-        }
-      ],
-      "run": "approved"
-    }
-  }
-}
-"""
+RULESET_NAME = "test"
 
 
-class Actions:  # NOTE: just one way of doing stuff
-    def approved(self, m):
-        print("Approved!")
+def get_rules_and_actions() -> tuple[dict, dict]:
+    with open("ruleset.json") as f:
+        content: dict = json.loads(f.read())
+    return content.get("ruleset"), content.get("actionsets")
 
-    def bla(self, m):
-        print("nja")
+
+# Dodaj novi fakt
+# Izmjeni vrednost postojećeg fakta (bla npr)
+# Obriši (reteract) fakt (blaa npr)
+
+
+def parse_action(action: dict) -> typing.Callable:
+    # XXX Factory goes here
+    match action.get("action"):
+        case "assert_fact":
+
+            def fn(m):
+                try:
+                    print("Asserting fact!")
+                    lang.get_host().assert_fact(
+                        RULESET_NAME,
+                        *action.get("payload"),
+                    )
+                except engine.MessageNotHandledException:
+                    ...  # TODO: Add logger
+
+            return fn
+
+        case "print":
+
+            def fn(m):
+                print("Printing payload!")
+                pprint.pprint(*action.get("payload"))
+
+            return fn
+
+        case _:
+            raise Exception("E jbg sad")
 
 
 class MyHost(engine.Host):
-    def __init__(self):
-        self.actions = Actions()
-        super().__init__(None)
+    def __init__(
+        self,
+        ruleset_definitions=None,
+        actionsets: dict = [],
+    ):
+        self.actionsets = actionsets
+        super().__init__(ruleset_definitions)
 
-    def get_action(self, action_name):
-        if hasattr(self.actions, action_name):
-            return getattr(self.actions, action_name)
-        return super().get_action(action_name)
+    def get_action(self, action_name: str):
+        actionset = [
+            parse_action(action)
+            for action in sorted(
+                self.actionsets.get(action_name),
+                key=lambda x: x.get("order"),
+            )
+        ]
+
+        def new_method(m):
+            for action in actionset:
+                action(m)
+
+        return new_method
+
+
+def run_rules():
+    ruleset, actionsets = get_rules_and_actions()
+    lang._main_host = MyHost(ruleset, actionsets)  # XXX Fuck that's ugly
+    lang.assert_fact(
+        RULESET_NAME,
+        {
+            "initial_value": 8,
+        },
+    )
 
 
 def main() -> None:
-    host = MyHost()
-    host.set_rulesets(json.loads(RULES))
-    host.post("test", {"subject": "World"})
+    run_rules()
 
 
 if __name__ == "__main__":
